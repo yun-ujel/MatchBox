57a0adf2ef675489a0503d88168e699ef3667ef0
yun-ujel <dhall2022@elam.co.uk>
2023-06-08 01:06:10 +0100

    Added ActionRebinder
    - Added ActionRebinder, ActionRebinderEditor and ActionRebinderDisplay based on the InputSystem sample for action rebinding
    
    - ActionRebinder is a MonoBehaviour script that uses public methods StartInteractiveRebind() and ResetToDefault() to configure overrides for a selected InputAction and binding
    (A.K.A. ActionRebinder rebinds input actions)
    
    - ActionRebinderEditor sets up the popup that allows you to pick a binding for the selected action
    
    - ActionRebinderDisplay uses events called by ActionRebinder to change TextMeshPro text, using the set keybinding
    
    All of this is groundwork and subject to change. Especially ActionRebinderDisplay. Things like the PlayerInput component and general input actions should still be safe to use.
    
    Other changes:
    
    - Reorganised Input Action Assets, they're now placed under Input\Action Assets
    - Updated InputTestScene to showcase new mono scripts

---
+++ Assets/Input/Rebinding/ActionRebinderDisplay.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class ActionRebinderDisplay : MonoBehaviour
+{
+    [SerializeField] private ActionRebinder actionRebinder;
+
+    [Space]
+
+    [SerializeField] private TextMeshProUGUI bindingText;
+
+    private void Awake()
+    {
+        actionRebinder.OnStartRebinding += OnRebind;
+        actionRebinder.OnStopRebinding += OnRebind;
+    }
+
+    private void Start()
+    {
+        UpdateDisplay(actionRebinder.InputAction, actionRebinder.BindingIndex);
+    }
+
+    private void OnRebind(object sender, ActionRebinder.InteractiveRebindingEventArgs args)
+    {
+        UpdateDisplay(args.action, args.bindingIndex);
+    }
+
+    private void UpdateDisplay(InputAction action, int bindingIndex)
+    {
+        if (action != null)
+        {
+            bindingText.text = action.GetBindingDisplayString(bindingIndex);
+        }
+    }
+}
