4c441c63e57c922332e7c7db02b4914cd5a3ec52
yun-ujel <113103357+yun-ujel@users.noreply.github.com>
2023-06-21 11:15:55 +0100

    Added GetWorldSize() method to MiscUtils
    - GetWorldSize() returns the world size of a given orthographic camera

--- Assets/MiscUtils.cs
+++ Assets/MiscUtils.cs
@@ -7,4 +7,16 @@ public static class MiscUtils
     {
         return Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
     }
+
+    public static Vector2 GetWorldSize(this Camera camera)
+    {
+        // Returns the Camera's Orthographic size converted to world units.
+
+        float screenToWorldHeight = 2 * camera.orthographicSize;
+        float screenToWorldWidth = screenToWorldHeight * camera.aspect;
+
+        //Debug.Log("Calculated World Size: " + new Vector2(screenToWorldWidth, screenToWorldHeight));
+
+        return new Vector2(screenToWorldWidth, screenToWorldHeight);
+    }
 }
