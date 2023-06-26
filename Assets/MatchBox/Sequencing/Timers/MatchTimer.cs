63194be4ba185dce6a90128cffb7219748bfc85f
yun-ujel <113103357+yun-ujel@users.noreply.github.com>
2023-06-26 11:49:36 +0100

    Started on MatchTimer
    MatchTimer is a timer that dictates how long the player spends on the "Match" part of the game, and runs down quicker as they find matches.

---
+++ Assets/MatchBox/Sequencing/Timers/MatchTimer.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using Grids;
+
+namespace MatchBox.Sequencing.Timers
+{
+    using Grids;
+
+    public class MatchTimer : MonoBehaviour
+    {
+        [SerializeField] private GridDisplay gridDisplay;
+
+        [Header("Timer")]
+        [SerializeField] private float startTime;
+        [SerializeField] private float timeLostPerMatch;
+
+        [Header("Debug")]
+        [SerializeField] private float displayedTimer;
+
+        private void Start()
+        {
+            gridDisplay.OnMatchFoundEvent += OnMatchFound;
+
+            displayedTimer = startTime;
+        }
+
+        private void OnMatchFound(object sender, GridDisplay.OnMatchFoundEventArgs e)
+        {
+            displayedTimer -= e.GridObjects.Length * timeLostPerMatch;
+        }
+
+        private void Update()
+        {
+            if (displayedTimer > 0f)
+            {
+                displayedTimer -= Time.deltaTime;
+            }
+            else
+            {
+                displayedTimer = 0f;
+                Debug.Log("Time's Up!");
+            }
+        }
+    }
+}
